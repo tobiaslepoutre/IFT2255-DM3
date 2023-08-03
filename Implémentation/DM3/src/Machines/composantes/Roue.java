e4837ffed9b022af233ab494e39d6ff4f6e0f480
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:30:31 +0200

    Update Roue.java

--- Implémentation/DM3/src/Machines/composantes/Roue.java
+++ Implémentation/DM3/src/Machines/composantes/Roue.java
@@ -1,5 +1,7 @@
 package Machines.composantes;
 
 public class Roue extends Composante {
-    
+    public Roue(String name, String type, String description, float price) {
+        super(name, type, description, price);
+    }
 }
