977651ec3f0f3f92063f67419c22589f5b7062b3
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:29:26 +0200

    Update HautParleur.java

--- Implémentation/DM3/src/Machines/composantes/HautParleur.java
+++ Implémentation/DM3/src/Machines/composantes/HautParleur.java
@@ -1,5 +1,7 @@
 package Machines.composantes;
 
 public class HautParleur extends Composante {
-    
+    public HautParleur(String name, String type, String description, float price) {
+        super(name, type, description, price);
+    }
 }
