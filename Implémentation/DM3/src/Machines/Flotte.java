c03adf188024a61b053718f83012b37a76152824
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:22:08 +0200

    Update Flotte.java

--- Implémentation/DM3/src/Machines/Flotte.java
+++ Implémentation/DM3/src/Machines/Flotte.java
@@ -1,5 +1,31 @@
 package Machines;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Flotte {
+    private List<Robot> robots;
+
+    public Flotte() {
+        this.robots = new ArrayList<>();
+    }
+
     
+    public void ajouterRobot(Robot robot) {
+        this.robots.add(robot);
+    }
+
+    public void supprimerRobot(Robot robot) {
+        this.robots.remove(robot);
+    }
+
+    public List<Robot> getRobots() {
+        return this.robots;
+    }
+
+    public void afficherEtat() {
+        for (Robot robot : this.robots) {
+            robot.afficherEtat();
+        }
+    }
 }
