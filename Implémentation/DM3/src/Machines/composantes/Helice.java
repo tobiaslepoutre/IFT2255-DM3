c4568886a467e0224b8b7c0cabff7422e0ed26fc
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:29:47 +0200

    Update Helice.java

--- Implémentation/DM3/src/Machines/composantes/Helice.java
+++ Implémentation/DM3/src/Machines/composantes/Helice.java
@@ -1,5 +1,7 @@
 package Machines.composantes;
 
 public class Helice extends Composante {
-    
+    public Helice(String name, String type, String description, float price) {
+        super(name, type, description, price);
+    }
 }
