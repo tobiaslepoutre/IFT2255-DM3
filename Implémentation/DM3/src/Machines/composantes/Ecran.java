bbe0fbf361a7136960ecee31f3966c8a705d873f
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:28:56 +0200

    Update Ecran.java

--- Implémentation/DM3/src/Machines/composantes/Ecran.java
+++ Implémentation/DM3/src/Machines/composantes/Ecran.java
@@ -1,6 +1,7 @@
 package Machines.composantes;
 
 public class Ecran extends Composante {
-    
+    public Ecran(String name, String type, String description, float price) {
+        super(name, type, description, price);
+    }
 }
-
