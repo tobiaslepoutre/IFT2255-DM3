a79a055b30281ba7a97ba35064121eaf460f7633
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:43:00 +0200

    Update Camera.java

--- Implémentation/DM3/src/Machines/composantes/Camera.java
+++ Implémentation/DM3/src/Machines/composantes/Camera.java
@@ -2,6 +2,6 @@ package Machines.composantes;
 
 public class Camera extends Composante {
     public Camera(String name, String type, String description, float price) {
-        super(name, type, description, price);
+        super(name, "Camera", description, price);
     }
 }
