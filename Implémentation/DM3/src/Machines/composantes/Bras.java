49092bd5db6d5a9ec1120e3b42e12432d59acca9
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:41:48 +0200

    Update Bras.java

--- Implémentation/DM3/src/Machines/composantes/Bras.java
+++ Implémentation/DM3/src/Machines/composantes/Bras.java
@@ -2,6 +2,6 @@ package Machines.composantes;
 
 public class Bras extends Composante {
     public Bras(String name, String type, String description, float price) {
-        super(name, type, description, price);
+        super(name, "Bras", description, price);
     }
 }
