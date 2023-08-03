f87f246e73e49766c9ff877c01d9fe66de453438
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:25:56 +0200

    Update Composante.java

--- Implémentation/DM3/src/Machines/composantes/Composante.java
+++ Implémentation/DM3/src/Machines/composantes/Composante.java
@@ -1,5 +1,48 @@
 package Machines.composantes;
 
-public abstract class Composante{
+public abstract class Composante {
 
+    private String name;
+    private String type;
+    private String description;
+    private float price;
+
+    public Composante(String name, String type, String description, float price) {
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getNameComp() {
+        return name;
+    }
+
+    public void setNameComp(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
