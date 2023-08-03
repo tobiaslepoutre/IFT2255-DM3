6f9c5eaf3fada2657c819e787bd5cf37c2762826
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:26:56 +0200

    Update CPU.java

--- Implémentation/DM3/src/Machines/composantes/CPU.java
+++ Implémentation/DM3/src/Machines/composantes/CPU.java
@@ -1,5 +1,19 @@
 package Machines.composantes;
 
 public class CPU extends Composante {
-    
+    private int consomation;
+
+    public CPU(String name, String type, String description, float price, int consomation) {
+        super(name, type, description, price); 
+        this.consomation = consomation;
+    }
+
+    public int getConsomation() {
+        return this.consomation;
+    }
+
+    public void setConsomation(int consomation) {
+        this.consomation = consomation;
+    }
+
 }
