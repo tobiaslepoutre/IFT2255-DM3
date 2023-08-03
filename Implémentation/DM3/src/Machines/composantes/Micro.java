15bc7c92888abb63e725bef9a1c5c099ad579140
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-03 22:44:55 +0200

    Update Micro.java

--- Implémentation/DM3/src/Machines/composantes/Micro.java
+++ Implémentation/DM3/src/Machines/composantes/Micro.java
@@ -2,6 +2,6 @@ package Machines.composantes;
 
 public class Micro extends Composante {
     public Micro(String name, String type, String description, float price) {
-        super(name, type, description, price);
+        super(name, "Micro", description, price);
     }
 }
