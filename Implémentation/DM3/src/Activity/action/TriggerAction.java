e1860d487a81086426bcf31179b2d36db9f02e2a
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-05 22:05:54 +0200

    Update TriggerAction.java w/ JavaDoc comments

--- Implémentation/DM3/src/Activity/action/TriggerAction.java
+++ Implémentation/DM3/src/Activity/action/TriggerAction.java
@@ -4,16 +4,40 @@ import Activity.Tache;
 
 import java.util.Date;
 
+/**
+ * La classe TriggerAction étend la classe Action et représente une action déclenchée.
+ * Cette classe a un attribut supplémentaire (executionDate) qui représente la date d'exécution de l'action.
+ * @author Giovanni Belval
+ * @version 1.0
+ */
 public class TriggerAction extends Action {
 
     private int executionDate;
 
+
+    /**
+     * Constructeur pour créer un objet TriggerAction avec la durée et la date d'exécution.
+     * Le type d'action est automatiquement défini par le nom de la classe.
+     * Notez que l'action n'est pas une transition (false est passé comme argument au super-constructeur).
+     *
+     * @param duration La durée de l'action.
+     * @param executionDate La date d'exécution de l'action.
+     */
     public TriggerAction(int duration , int executionDate){
         super(duration, false);
         this.executionDate = executionDate;
         this.type = this.getClass().getSimpleName();
     }
 
+    /**
+     * Constructeur pour créer un objet TriggerAction avec la durée, la tâche associée et la date d'exécution.
+     * Le type d'action est automatiquement défini par le nom de la classe.
+     * Notez que l'action n'est pas une transition (false est passé comme argument au super-constructeur).
+     *
+     * @param duration La durée de l'action.
+     * @param task La tâche associée à l'action.
+     * @param executionDate La date d'exécution de l'action.
+     */
     public TriggerAction(int duration , Tache task, int executionDate){
         super(duration, false, task);
         this.executionDate = executionDate;
