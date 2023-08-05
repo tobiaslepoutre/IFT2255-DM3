b4dbe551673e17b00b5df198f56768728e0130ee
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-05 22:03:30 +0200

    Update TranslateAction.java w/ JavaDoc comments

--- Implémentation/DM3/src/Activity/action/TranslateAction.java
+++ Implémentation/DM3/src/Activity/action/TranslateAction.java
@@ -2,6 +2,13 @@ package Activity.action;
 
 import Activity.Tache;
 
+/**
+ * La classe TranslateAction étend la classe Action et représente une action de translation.
+ * Cette classe a des attributs supplémentaires (x, y, z, speed) qui représentent respectivement
+ * les déplacements en x, y, et z, et la vitesse de déplacement.
+ * @author Giovanni Belval
+ * @version 1.0
+ */
 public class TranslateAction extends Action {
 
     private float x;
@@ -10,6 +17,18 @@ public class TranslateAction extends Action {
 
     private float speed;
 
+    /**
+     * Constructeur pour créer un objet TranslateAction avec la durée, une indication si c'est une action de transition,
+     * les déplacements en x, y, z et la vitesse de déplacement.
+     * Le type d'action est automatiquement défini par le nom de la classe.
+     *
+     * @param duration La durée de l'action.
+     * @param transition Indique si l'action est une transition.
+     * @param x Déplacement en x.
+     * @param y Déplacement en y.
+     * @param z Déplacement en z.
+     * @param speed La vitesse de déplacement.
+     */
     public TranslateAction(int duration, boolean transition, float x , float y , float z , float speed){
         super(duration, transition);
         this.x = x;
@@ -19,6 +38,19 @@ public class TranslateAction extends Action {
         this.type = this.getClass().getSimpleName();
     }
 
+    /**
+     * Constructeur pour créer un objet TranslateAction avec la durée, une indication si c'est une action de transition, la tâche associée,
+     * les déplacements en x, y, z et la vitesse de déplacement.
+     * Le type d'action est automatiquement défini par le nom de la classe.
+     *
+     * @param duration La durée de l'action.
+     * @param transition Indique si l'action est une transition.
+     * @param task La tâche associée à l'action.
+     * @param x Déplacement en x.
+     * @param y Déplacement en y.
+     * @param z Déplacement en z.
+     * @param speed La vitesse de déplacement.
+     */
     public TranslateAction(int duration, boolean transition, Tache task, float x , float y , float z , float speed){
         super(duration, transition, task);
         this.x = x;
