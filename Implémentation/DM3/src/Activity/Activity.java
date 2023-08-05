85ad6de6d601aeb2214698f558447bf2f26faa19
Tobias Lepoutre <115952383+tobiaslepoutre@users.noreply.github.com>
2023-08-05 21:14:05 +0200

    Update Activity.java w/ JavaDoc comments

--- Implémentation/DM3/src/Activity/Activity.java
+++ Implémentation/DM3/src/Activity/Activity.java
@@ -5,22 +5,36 @@ import Users.Utilisateur;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * La classe Activity représente une activité dans le système.
+ * @author Giovanni Belval
+ * @version 1.0
+ */
 public class Activity{
 
-    /* attributes*/
+    /* Attributs*/
     private int reward;
     private Date startDate;
     private Date endDate;
     private String name;
 
-    /* assosiations */
+    /* Associations */
     private ArrayList<Utilisateur> participants;
     private Utilisateur createur;
     private ArrayList<Robot> robots;
     private ArrayList<Interet> interets;
     private ArrayList<Tache> taches;
 
-    /* Constructors */
+    /**
+     * Constructeur pour l'objet Activity.
+     *
+     * @param reward Le gain pour l'activité.
+     * @param startDate La date de début de l'activité.
+     * @param endDate La date de fin de l'activité.
+     * @param name Le nom de l'activité.
+     * @param createur Le créateur de l'activité.
+     * @param interet L'interet lié à l'activité.
+     */
     public Activity(int reward, Date startDate , Date endDate , String name, Utilisateur createur, Interet interet)  {
 
         this.reward = reward;
@@ -41,56 +55,99 @@ public class Activity{
 
     }
 
+    /**
+     * Ajoute un robot participant à l'activité.
+     *
+     * @param r Le robot à ajouter.
+     */
     public void addParticipantRobot(Robot r){
         if(!this.robots.contains(r)){
             this.robots.add(r);
         }
     }
 
+
+    /**
+     * Ajoute un utilisateur participant à l'activité.
+     *
+     * @param user L'utilisateur à ajouter.
+     */
     public void addParticipant(Utilisateur user){
         if(!this.participants.contains(user)){
             this.participants.add(user);
         }
     }
 
+    /**
+     * Attribue les récompenses aux participants de l'activité.
+     */
     public void giveReward(){
         for(Utilisateur user : this.participants){
             user.addPoints(this.reward);
         }
     }
 
+    /**
+     * Notifie tous les participants que l'activité se termine bientôt.
+     */
     public void notifierDateLimite(){
         for(Utilisateur user : this.participants){
             user.addNotification("the activity "+ this.name + "is ending soon : " + this.endDate);
         }
     }
 
-
+    /**
+     * Renvoie le nom de l'activité.
+     *
+     * @return Le nom de l'activité.
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * Renvoie la date de début de l'activité.
+     *
+     * @return La date de début.
+     */
     public Date getStartDate(){
         return this.startDate;
     }
 
+    /**
+     * Renvoie la date de fin de l'activité.
+     *
+     * @return La date de fin.
+     */
     public Date getEndDate(){
         return this.endDate;
     }
 
+    /**
+     * Renvoie la liste des utilisateurs participants à l'activité.
+     *
+     * @return La liste des utilisateurs participants.
+     */
     public ArrayList<Utilisateur> getParticipants(){
         return this.participants;
     }
 
+    /**
+     * Renvoie la liste des robots participants à l'activité.
+     *
+     * @return La liste des robots participants.
+     */
     public ArrayList<Robot> getParticipantsRobot(){
         return this.robots;
     }
 
+    /**
+     * Renvoie le créateur de l'activité.
+     *
+     * @return Le créateur de l'activité.
+     */
     public Utilisateur getCreator(){
         return this.createur;
     }
-
-
-
-
+    
 }
